ff37ff152311388687f9cbb2cd700de7511835ad
Tezera <tezerac@gmail.com>
2020-10-03 03:37:17 -0700

    new-xaml created

---
+++ UWP_LibraryAPP/MusicLibrary/Music.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicLibrary
+{
+    public class Music
+    {        
+        public string title { get; set; }
+        public string artistName { get; set; }
+        public string genra { get; set; }
+        public string album { get; set; }
+       
+        public static Music GetMusic()
+        {
+            var music = new Music()
+            {
+                title = "Tikur Sew",
+                artistName = "Tedi Afro",
+                genra = "Rege",
+                album = "Menilik"
+            };
+            return music;
+        }
+    }
+}
